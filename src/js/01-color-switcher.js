73b9ad4ae8479a27021828d1086ae34c3153edc0
BartoszBaranski90 <bartosz.baranski1990@gmail.com>
2023-08-29 11:16:51 +0200

    maybe finish taskOne

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const bodyView = document.body;
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+btnStop.disabled = true;
+
+let timeToChange = null;
+
+function btnSwitchMode({ trueOrder = true } = {}) {
+  btnStart.disabled = trueOrder;
+  trueOrder = !trueOrder;
+  btnStop.disabled = trueOrder;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bcgColorChange() {
+  document.body.style.background = getRandomHexColor();
+}
+
+btnStart.addEventListener('click', () => {
+  bcgColorChange(), (timeToChange = setInterval(bcgColorChange, 1000));
+  btnSwitchMode();
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timeToChange);
+  btnSwitchMode({ trueOrder: false });
+});

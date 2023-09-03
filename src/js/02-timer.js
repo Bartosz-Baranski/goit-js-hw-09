0b46a663595adb6c449357ab7aae1ea070ce9836
BartoszBaranski90 <bartosz.baranski1990@gmail.com>
2023-09-03 14:08:17 +0200

    task 3 complit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -36,18 +36,19 @@ function updateValue(days, hours, minutes, seconds) {
 }
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // days
+  // Remaining days
   const days = Math.floor(ms / day);
-  // hours
+  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // minutes
+  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // seconds
+  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };

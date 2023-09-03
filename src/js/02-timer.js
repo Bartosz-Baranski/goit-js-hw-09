f28360c6902cf123f2f54494bbe739c2670ba9b6
BartoszBaranski90 <bartosz.baranski1990@gmail.com>
2023-09-03 13:58:20 +0200

    something wrpng with task two

---
+++ src/js/02-timer.js
@@ -0,0 +1,77 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const btnStart = document.querySelector('button[data-start]');
+
+btnStart.disabled = true;
+
+let futureDate;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultData: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+      btnStart.disabled = true;
+      return;
+    }
+    btnStart.disabled = false;
+  },
+};
+flatpickr('#datetime-picker', options);
+let days = 0;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+function updateValue(days, hours, minutes, seconds) {
+  document.querySelector('.value[data-days]').textContent = `${days}`;
+  document.querySelector('.value[data-hours]').textContent = `${hours}`;
+  document.querySelector('.value[data-minutes]').textContent = `${minutes}`;
+  document.querySelector('.value[data-seconds]').textContent = `${seconds}`;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // days
+  const days = Math.floor(ms / day);
+  // hours
+  const hours = Math.floor((ms % day) / hour);
+  // minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return `${value}`.padStart(2, '0');
+}
+
+btnStart.addEventListener('click', function () {
+  const future = new Date(futureDate);
+
+  setInterval(function () {
+    const now = new Date();
+    const distance = future - now;
+    if (distance <= 0) {
+      return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(distance);
+    updateValue(
+      addLeadingZero(days),
+      addLeadingZero(hours),
+      addLeadingZero(minutes),
+      addLeadingZero(seconds)
+    );
+  }, 1000);
+});
